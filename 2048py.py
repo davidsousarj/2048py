3c25a2801cc571c9a882d1456e6c18f943fbecd9
davidsousarj <david.sousarj@yahoo.com.br>
2015-08-24 17:46:14 -0300

    Update 2048py.py

--- 2048py.py
+++ 2048py.py
@@ -1,7 +1,14 @@
-# 2048.py
+# 2048.py 
+# Aug 22, 2015
 # Written in python / pygame by DavidSousaRJ - david.sousarj@gmail.com
 # License: Creative Commons
 # Sorry about some comments in portuguese!
+#
+# CHANGES:
+# Aug 24 - fixed colors /fonts
+# BUG: game ending not working
+# BUG: when a play is not possible it keeps adding a random tile
+#
 import os
 import sys
 import pygame
@@ -147,28 +154,43 @@ margin = 5
 thickness = 0
 STATUS=0
 
-color1=(100,100,100)
-color2=(200,200,200)
+colorback=(189,174,158)
+colorblank=(205,193,180)
+colorlight=(249,246,242)
+colordark=(119,110,101)
 
-dictcolor={
-0:color2,
-2:(150,150,150),
-4:(180,180,180),
-8:(255,200,200),
-16:(255,100,100),
-32:(255,50,50),
-64:(255,0,0),
-128:(255,255,50),
-256:(255,255,100),
-512:(255,255,150),
-1024:(255,255,200),
-2048:(255,255,255) }
+dictcolor1={
+0:colorblank,
+2:(238,228,218),
+4:(237,224,200),
+8:(242,177,121),
+16:(245,149,99),
+32:(246,124,95),
+64:(246,95,59),
+128:(237,207,114),
+256:(237,204,97),
+512:(237,200,80),
+1024:(237,197,63),
+2048:(237,194,46) }
+
+dictcolor2={
+2:colordark,
+4:colordark,
+8:colorlight,
+16:colorlight,
+32:colorlight,
+64:colorlight,
+128:colorlight,
+256:colorlight,
+512:colorlight,
+1024:colorlight,
+2048:colorlight }
 
 # Init screen
 pygame.init()
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption( 'Python 2048 by DavidSousaRJ' )
-myfont = pygame.font.SysFont("Impact", 30)
+myfont = pygame.font.SysFont("Arial", 30)
 
 def gameover(STATUS):
 	if STATUS == 1:
@@ -180,16 +202,16 @@ def gameover(STATUS):
 	pygame.display.update()
 
 def show(TABLE):
-	screen.fill(color1)
+	screen.fill(colorback)
 	for i in range(4):
 		for j in range(4):
-			pygame.draw.rect(screen, dictcolor[TABLE[i][j]], (j*boxsize+margin,
+			pygame.draw.rect(screen, dictcolor1[TABLE[i][j]], (j*boxsize+margin,
 											  i*boxsize+margin,
 											  boxsize-2*margin,
 											  boxsize-2*margin),
 											  thickness)
 			if TABLE[i][j] != 0:
-				label = myfont.render("%4s" %(TABLE[i][j]), 1, (255,255,255))
+				label = myfont.render("%4s" %(TABLE[i][j]), 1, dictcolor2[TABLE[i][j]] )
 				screen.blit(label, (j*boxsize+4*margin, i*boxsize+5*margin))
 	pygame.display.update()
 
